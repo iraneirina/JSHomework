726cfc928fd946da0f31f130cd5fef88a54f99dc
lebedevairina <80787293+iraneirina@users.noreply.github.com>
2022-06-25 15:01:38 +0300

    hw7

---
+++ hw8/src/burger.js
@@ -0,0 +1,7 @@
+'use strict';
+const openMenuBtn = document.querySelector('.button-burger');
+const mainMenuEl = document.querySelector('.main-menu');
+
+openMenuBtn.addEventListener('click', function () {
+  mainMenuEl.classList.toggle('hidden');
+});
